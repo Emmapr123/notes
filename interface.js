f0b2283c779ba0635f3fd24f0dd59f6027b97fa9
Emmapr123 <emmapriester@hotmail.com>
2021-04-14 15:16:12 +0100

    temptemp

--- interface.js
+++ interface.js
@@ -8,23 +8,21 @@ document.querySelector(".add-note").addEventListener("click", refreshRight) ;
 function refreshRight() {
   console.log("clicked")
   document.querySelector(".right-inner").innerHTML= "<div class='buttons'><div></div><button class='save-edit-delete-note' id='delete'>DELETE</button><button class='save-edit-delete-note' id='save'>SAVE</button></div><input type='text' placeholder='TITLE' name='title' id='title'><textarea id='note' rows='4' cols='50' placeholder='  NOTE'></textarea></div>";
-  // document.querySelector(".right-inner").innerHTML= "<h1>test</h1>";
   document.querySelector("#save").addEventListener("click", addNewNote) ;
 }
 
 function addNewNote() {
-  console.log("Also clicked")
   var title = document.querySelector("#title").value
   var note = document.querySelector("#note").value
 
   postData('https://makers-emojify.herokuapp.com/', { text: note })
     .then(data => {
-      note = data; // JSON data parsed by `data.json()` call
-      var newNote = noteManager.add(title, note)
-      console.log(newNote)
+      note = data;
+      noteManager.add(title, note)
+      localStorage.setItem(title, note)
       listNotes();
-    });
-    
+    });  
+  
 }
 
 function listNotes() {
